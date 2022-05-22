1d4f1b59392f0a2dda37deb7d05c3fd035a98721
Candy <candychen911@gmail.com>
2022-05-22 17:34:44 +0800

    Refactor code into pipeline design pattern

---
+++ yt_concate/main.py
@@ -0,0 +1,24 @@
+from yt_concate.pipeline.steps.get_video_list import GetVideoList
+from yt_concate.pipeline.steps.step import StepException
+
+from yt_concate.pipeline.pipeline import Pipeline
+
+channel_id = 'UCKSVUHI9rbbkXhvAXK-2uxA'
+
+
+def main():
+    inputs = {
+        'channel_id': channel_id
+    }
+
+    steps = [
+        GetVideoList(),
+        ]
+
+    p = Pipeline(steps)
+    p.run(inputs)
+
+
+if __name__ == '__main__':
+    main()
+
